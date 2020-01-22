f337cd240bd0828e865734da9bfa7f1b9641e242
davitasryan3 <davitasryan3@outlook.com>
2020-01-22 23:37:11 +0400

    remove unnccessary folders

--- Source/DataAccess/IDEntityBase.cs
+++
@@ -1,7 +0,0 @@
-namespace DataAccess
-{
-    public abstract class IDEntityBase<T> : IIdEntityBase<T>
-    {
-        public T ID { get; set; }
-    }
-}
--- Source/DataAccess/IIDEntityBase.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DataAccess
-{
-    public interface IIdEntityBase<T>
-    {
-        T ID { get; set; }
-    }
-}
