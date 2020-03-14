014907b13693e226d90042c6724725339e50c5fd
davitasryan3 <davitasryan3@outlook.com>
2020-03-14 13:35:53 +0400

    Change names

--- Source/Arcade.Desktop/Shell/Properties/AssemblyInfo.cs
+++ Source/Arcade.Desktop/Shell/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Shell")]
+[assembly: AssemblyTitle("Queue Display")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Shell")]
+[assembly: AssemblyProduct("QueueDisplay")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
